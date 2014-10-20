63bd0b28a63b0c82404a3e9be5b6794b2be102cd
Damon Barry <damonbarry@outlook.com>
2014-10-19 19:41:20 -0700

    tokens container is not empty when a string with a valid token is passed to it

--- spec/can_recognize_tokens.cpp
+++ spec/can_recognize_tokens.cpp
@@ -1,27 +1,51 @@
 #include "CppUnitTest.h"
+#include <string>
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace algo
 {
+    template<class T, class Tokenize>
     class tokens
     {
+        vector<T> tokens_;
+        Tokenize tokenize_;
     public:
-        bool empty() { return true; }
+        explicit tokens(Tokenize fn) : tokenize_(fn) {}
+        bool empty() const { return tokens_.size() == 0; }
+        void from_string(string s)
+        {
+            tokens_.push_back(tokenize_(s));
+        }
     };
+
+    template<class T, class Tokenize>
+    tokens<T, Tokenize> make_tokens(Tokenize&& fn) { return tokens<T, Tokenize>(std::forward<Tokenize>(fn)); }
 }
 
 namespace algo { namespace spec
-{		
+{
+    struct s {};
+    auto alwaysReturnAToken = [](string&){ return s(); };
+
     TEST_CLASS(can_recognize_tokens)
 	{
 	public:
-		
+
         TEST_METHOD(should_not_have_any_tokens_when_default_constructed)
 		{
-            tokens testSubject;
-            Assert::IsTrue(testSubject.empty());
+            auto testTokens = make_tokens<s>(alwaysReturnAToken);
+            Assert::IsTrue(testTokens.empty());
 		}
+
+        TEST_METHOD(should_recognize_a_token)
+        {
+            auto testTokens = make_tokens<s>(alwaysReturnAToken);
+            testTokens.from_string("s");
+            Assert::IsFalse(testTokens.empty());
+        }
 
 	};
 }}
