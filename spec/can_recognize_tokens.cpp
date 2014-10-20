cb845977d585c88aab26fb1ab131a49e7b5c565d
Damon Barry <damonbarry@outlook.com>
2014-10-19 20:37:20 -0700

    container has N tokens when a string with N tokens is passed to it

--- spec/can_recognize_tokens.cpp
+++ spec/can_recognize_tokens.cpp
@@ -1,6 +1,7 @@
 #include "CppUnitTest.h"
 #include <string>
 #include <vector>
+#include <array>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -13,23 +14,27 @@ namespace algo
         vector<typename Traits::type> tokens_;
     public:
         bool empty() const { return tokens_.size() == 0; }
+        size_t size() const { return tokens_.size(); }
         void from_string(string s)
         {
-            tokens_.push_back(Traits::tokenize(s));
+            auto newTokens = Traits::tokenize(s);
+            tokens_.insert(tokens_.end(), newTokens.begin(), newTokens.end());
         }
     };
 }
 
 namespace algo { namespace spec
 {
+    template<size_t N>
     struct test_token_traits
     {
         struct s {};
         typedef s type;
-        static s tokenize(const string&) { return s(); }
+        static array<s, N> tokenize(const string&) { return array<s, N>(); }
     };
 
-    typedef tokens<test_token_traits> test_tokens;
+    template<size_t N>
+    using test_tokens = tokens<test_token_traits<N>>;
 
     TEST_CLASS(can_recognize_tokens)
 	{
@@ -37,15 +42,22 @@ namespace algo { namespace spec
 
         TEST_METHOD(should_not_have_any_tokens_when_default_constructed)
 		{
-            test_tokens testTokens;
+            test_tokens<0> testTokens;
             Assert::IsTrue(testTokens.empty());
 		}
 
         TEST_METHOD(should_recognize_a_token_in_a_string)
         {
-            test_tokens testTokens;
-            testTokens.from_string("s");
+            test_tokens<1> testTokens;
+            testTokens.from_string("irrelevant");
             Assert::IsFalse(testTokens.empty());
+        }
+
+        TEST_METHOD(should_recognize_more_than_one_token_in_a_string)
+        {
+            test_tokens<3> testTokens;
+            testTokens.from_string("irrelevant");
+            Assert::AreEqual(3U, testTokens.size());
         }
 
 	};
