ca4d05c7948a3b172442961e8cee6cd102b46675
Damon Barry <damonbarry@outlook.com>
2014-11-04 11:01:46 -0800

    add fluent-style assertions for terminal tests

--- spec/can_recognize_ascii_tree_chars.cpp
+++ spec/can_recognize_ascii_tree_chars.cpp
@@ -6,6 +6,19 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
+namespace
+{
+    class terminal_assertions
+    {
+        const terminal& term_;
+    public:
+        explicit terminal_assertions(const terminal& term) : term_(term) {}
+        void should_be(const terminal& other, const wchar_t* message = nullptr) { Assert::AreEqual(other, term_, message); }
+    };
+
+    terminal_assertions _(const terminal& term) { return terminal_assertions(term); }
+}
+
 namespace ascii_tree { namespace spec
 {
     TEST_CLASS(can_recognize_ascii_tree_chars)
@@ -14,19 +27,19 @@ namespace ascii_tree { namespace spec
         TEST_METHOD(should_recognize_an_opening_square_brace)
         {
             terminal term = terminal_traits::to_terminal('[');
-            Assert::AreEqual(open_square_brace, term);
+            _(term).should_be(open_square_brace);
         }
 
         TEST_METHOD(should_recognize_an_asterisk)
         {
             terminal term = terminal_traits::to_terminal('*');
-            Assert::AreEqual(asterisk, term);
+            _(term).should_be(asterisk);
         }
 
         TEST_METHOD(should_recognize_a_closing_square_brace)
         {
             terminal term = terminal_traits::to_terminal(']');
-            Assert::AreEqual(close_square_brace, term);
+            _(term).should_be(close_square_brace);
         }
 
         TEST_METHOD(should_recognize_a_name_char)
@@ -40,56 +53,56 @@ namespace ascii_tree { namespace spec
             for (auto ch : name_chars)
             {
                 terminal term = terminal_traits::to_terminal(ch);
-                Assert::AreEqual(name_char, term, wstring(&ch, &ch + 1).c_str());
+                _(term).should_be(name_char, wstring(&ch, &ch + 1).c_str());
             }
         }
 
         TEST_METHOD(should_recognize_a_dash)
         {
             terminal term = terminal_traits::to_terminal('-');
-            Assert::AreEqual(dash, term);
+            _(term).should_be(dash);
         }
 
         TEST_METHOD(should_recognize_a_opening_paren)
         {
             terminal term = terminal_traits::to_terminal('(');
-            Assert::AreEqual(open_paren, term);
+            _(term).should_be(open_paren);
         }
 
         TEST_METHOD(should_recognize_a_closing_paren)
         {
             terminal term = terminal_traits::to_terminal(')');
-            Assert::AreEqual(close_paren, term);
+            _(term).should_be(close_paren);
         }
 
         TEST_METHOD(should_recognize_a_backslash)
         {
             terminal term = terminal_traits::to_terminal('\\');
-            Assert::AreEqual(backslash, term);
+            _(term).should_be(backslash);
         }
 
         TEST_METHOD(should_recognize_a_pipe)
         {
             terminal term = terminal_traits::to_terminal('|');
-            Assert::AreEqual(pipe, term);
+            _(term).should_be(pipe);
         }
 
         TEST_METHOD(should_recognize_a_slash)
         {
             terminal term = terminal_traits::to_terminal('/');
-            Assert::AreEqual(slash, term);
+            _(term).should_be(slash);
         }
 
         TEST_METHOD(should_recognize_a_space)
         {
             terminal term = terminal_traits::to_terminal(' ');
-            Assert::AreEqual(space, term);
+            _(term).should_be(space);
         }
 
         TEST_METHOD(should_not_recognize_an_invalid_char)
         {
             terminal term = terminal_traits::to_terminal('~');
-            Assert::AreEqual(none, term);
+            _(term).should_be(none);
         }
 
     };
