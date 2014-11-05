97648cee1ff048710053b2c66af234cf9ef4216d
Damon Barry <damon.barry@microsoft.com>
2014-11-04 17:55:33 -0800

    use fluent-style assertions in can_recognize_tokens tests

--- spec/can_recognize_tokens.cpp
+++ spec/can_recognize_tokens.cpp
@@ -1,9 +1,8 @@
 #include "tokens.hpp"
-#include <CppUnitTest.h>
+#include "test_helpers.hpp"
 #include <string>
 #include <array>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
 namespace ascii_tree { namespace spec
@@ -27,28 +26,28 @@ namespace ascii_tree { namespace spec
         TEST_METHOD(should_not_have_any_tokens_when_default_constructed)
         {
             test_tokens<0> testTokens;
-            Assert::IsTrue(testTokens.empty());
+            _(testTokens.empty()).should_be_true();
         }
 
         TEST_METHOD(should_recognize_a_token_in_a_string)
         {
             test_tokens<1> testTokens;
             testTokens.from_string("irrelevant");
-            Assert::AreEqual(1U, testTokens.size());
+            _(testTokens.size()).should_be(1U);
         }
 
         TEST_METHOD(should_recognize_more_than_one_token_in_a_string)
         {
             test_tokens<3> testTokens;
             testTokens.from_string("irrelevant");
-            Assert::AreEqual(3U, testTokens.size());
+            _(testTokens.size()).should_be(3U);
         }
 
         TEST_METHOD(should_accumulate_tokens_from_successive_strings)
         {
             test_tokens<2> testTokens(3);
             testTokens.from_string("irrelevant");
-            Assert::AreEqual(5U, testTokens.size());
+            _(testTokens.size()).should_be(5U);
         }
 
         TEST_METHOD(should_be_able_to_enumerate_tokens)
@@ -56,7 +55,7 @@ namespace ascii_tree { namespace spec
             test_tokens<0> testTokens(3);
             size_t i = 0;
             for (s tok : testTokens) { ++i; }
-            Assert::AreEqual(3U, i);
+            _(i).should_be(3U);
         }
 
         TEST_METHOD(should_be_able_to_enumerate_const_tokens_from_a_const_container)
@@ -64,7 +63,7 @@ namespace ascii_tree { namespace spec
             const test_tokens<0> testTokens(3);
             size_t i = 0;
             for (const s tok : testTokens) { ++i; }
-            Assert::AreEqual(3U, i);
+            _(i).should_be(3U);
         }
 
         TEST_METHOD(should_be_able_to_enumerate_const_tokens_from_a_non_const_container)
@@ -73,21 +72,21 @@ namespace ascii_tree { namespace spec
             test_tokens<0> testTokens(3);
             size_t i = 0;
             for (const_iter it = testTokens.cbegin(); it != testTokens.cend(); ++it) { ++i; }
-            Assert::AreEqual(3U, i);
+            _(i).should_be(3U);
         }
 
         TEST_METHOD(should_be_able_to_access_tokens_by_their_index)
         {
             test_tokens<0> testTokens = { { "one" }, { "two" }, { "three" } };
-            Assert::AreEqual("one", testTokens[0].val.c_str());
-            Assert::AreEqual("two", testTokens[1].val.c_str());
-            Assert::AreEqual("three", testTokens[2].val.c_str());
+            _(testTokens[0].val).should_be("one");
+            _(testTokens[1].val).should_be("two");
+            _(testTokens[2].val).should_be("three");
         }
 
         TEST_METHOD(should_be_able_to_access_tokens_from_a_const_container_by_ther_index)
         {
             const test_tokens<0> testTokens = { { "one" } };
-            Assert::AreEqual("one", testTokens[0].val.c_str());
+            _(testTokens[0].val).should_be("one");
         }
 
     };
